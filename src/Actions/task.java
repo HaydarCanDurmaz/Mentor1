5377d0891253363b538b7a393c1aeb8f2b0af76c
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-03-14 15:51:11 +0300

    senaryo 3

--- src/Actions/task.java
+++ src/Actions/task.java
@@ -8,38 +8,36 @@ import org.openqa.selenium.interactions.Actions;
 
 public class task extends BaseDriver {
     @Test
-    public void senaryo1(){
+    public void senaryo1() {
         driver.get("https://www.browserstack.com/");
 
-        Actions aksiyonlar=new Actions(driver);
-        WebElement getStarted= driver.findElement(By.linkText("Get started free"));
+        Actions aksiyonlar = new Actions(driver);
+        WebElement getStarted = driver.findElement(By.linkText("Get started free"));
         aksiyonlar.moveToElement(getStarted).click().build().perform();
 
 
+    }
 
-
-
-
-
-
-
-
-    } @Test
-    public void senaryo2(){
+    @Test
+    public void senaryo2() {
         driver.get("https://www.browserstack.com/");
 
-        Actions aksiyonlar=new Actions(driver);
-        WebElement live= driver.findElement(By.xpath("//a[@class='product-cards-wrapper--click--hoverclick product-card-live']"));
-        aksiyonlar.moveToElement(live).click().build().perform();
+        Actions aksiyonlar = new Actions(driver);
+        WebElement live = driver.findElement(By.xpath("//a[@class='product-cards-wrapper--click--hoverclick product-card-live']"));
+        aksiyonlar.moveToElement(live).build().perform();
 
-
-
-
-
-
-
-
+        WebElement automat = driver.findElement(By.xpath("//a[@class='product-card-app-automate product-cards-wrapper--click--hoverclick']"));
+        aksiyonlar.moveToElement(automat).click().build().perform();
 
         BekleKapat();
 
-}}
+    }
+    @Test
+    public void senaryo3() {
+        driver.get("https://www.browserstack.com/");
+
+        Actions aksiyonlar = new Actions(driver);
+        WebElement freetrial = driver.findElement(By.xpath("//a[@class='btn-secondary-white btn-md']"));
+        aksiyonlar.doubleClick(freetrial).build().perform();
+    }
+}
